f7cc2c98ea44dd775462e5495f27e4881dbd538f
andersongames <ander.cal45@gmail.com>
2020-06-27 11:23:03 -0300

    Exemplo Aula 3

---
+++ Aula3/src/laboratorio/ContaCorrente.java
@@ -0,0 +1,23 @@
+package laboratorio;
+
+public class ContaCorrente extends Conta {
+	public ContaCorrente() {
+		super(10000);
+	}
+	
+	public double saque (double valor) {
+		if (valor < saldo + limite) {
+			saldo = saldo + limite - valor;
+		}
+		return saldo;
+	}
+	
+	public static void main(String[] args) {
+		ContaCorrente cc = new ContaCorrente();
+		System.out.println(cc.saldo());
+		cc.deposito(1000);
+		System.out.println(cc.saldo());
+		cc.saque(6000);
+		System.out.println(cc.saldo());
+	}
+}
