37e006bc99694a7a64b15fe1d922215c6f0983f6
andersongames <ander.cal45@gmail.com>
2020-07-05 12:29:13 -0300

    Aula 6
    Polimorfismo

---
+++ Aula6/src/laboratorio/Circulo.java
@@ -0,0 +1,12 @@
+package laboratorio;
+
+public class Circulo extends FiguraGeometrica {
+	public Circulo (double raio) {
+		super(raio, 0);
+	}
+	
+	@Override
+	public double calculaArea() {
+		return 2 * Math.PI * lado1;
+	}
+}
